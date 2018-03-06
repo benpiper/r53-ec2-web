1dcb03c8072cd8059130ed55c931c62d96540741
Ben Piper <ben@benpiper.com>
2018-03-06 16:44:48 -0500

    Call counter function

--- src/index.php
+++ src/index.php
@@ -1,8 +1,10 @@
 <?php
+include("counter.php");
+
 echo "Hostname: " . gethostname() . "<br>";
 echo "Public IP: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-ipv4') . "<br>";
 echo "Host headers: " . $_SERVER['HTTP_HOST'] . "<br>";
 echo "Client IP: " . $_SERVER['REMOTE_ADDR'] . "<br>";
-echo "Hits: " . include("counter.php");
+echo "Hits: " . counter("countlog.txt");
 //phpinfo();
 ?>
