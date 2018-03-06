fe5b18bfee663877664fc3361cf1dba008f42c28
Ben Piper <ben@benpiper.com>
2018-03-06 14:22:46 -0500

    Fix formatting and counter

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 echo "Hostname: " . gethostname() . "<br>";
-echo file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-ipv4');
+echo "Public IP: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-ipv4') . "<br>";
 echo "Host headers: " . $_SERVER['HTTP_HOST'] . "<br>";
 echo "Client IP: " . $_SERVER['REMOTE_ADDR'] . "<br>";
-include("counter.php");
+echo "Hits: " . include("counter.php");
 //phpinfo();
