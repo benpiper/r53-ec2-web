2915d2a49efdc4fe4ea8df6f2e85f07709a58ad6
Ben Piper <ben@benpiper.com>
2018-03-06 15:33:30 -0500

    Add binary flag

--- src/counter.php
+++ src/counter.php
@@ -4,11 +4,11 @@
 $datei = fopen("countlog.txt","r");
 $count = fgets($datei,1000);
 fclose($datei);
-$count=$count + 1 ;
-echo "$count" ;
+$count=$count+1;
+echo "$count";
 
 // opens countlog.txt to change new hit number
-$datei = fopen("countlog.txt","w");
+$datei = fopen("countlog.txt","wb");
 fwrite($datei, $count);
 fclose($datei);
 
