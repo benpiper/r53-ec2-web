f731bcb17d1c66ee2b89723927fe993c38c1938e
Ben Piper <ben@benpiper.com>
2018-03-06 14:03:39 -0500

    Display public IP and host headers. Fix counter.

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 echo "Hostname: " . gethostname() . "<br>";
-echo "Headers: " . get_headers() . "<br>";
+echo file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-ipv4');
+echo "Host headers: " . $_SERVER['HTTP_HOST'] . "<br>";
 echo "Client IP: " . $_SERVER['REMOTE_ADDR'] . "<br>";
-include("/counter.php");
+include("counter.php");
 //phpinfo();
