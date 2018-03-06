70cb728e0b414c5626de6bf82cb38d7a4dc708b7
Ben Piper <ben@benpiper.com>
2018-03-06 13:04:37 -0500

    Display server and client info and visitor count

--- src/index.php
+++ src/index.php
@@ -1,2 +1,6 @@
 <?php
-phpinfo();
+echo "Hostname: " . gethostname() . "<br>";
+echo "Headers: " . get_headers() . "<br>";
+echo "Client IP: " . $_SERVER['REMOTE_ADDR'] . "<br>";
+include("/counter.php");
+//phpinfo();
