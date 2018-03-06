2b4a1244b81e5edb936610f1b9bf518410b8d272
Ben Piper <ben@benpiper.com>
2018-03-06 16:44:27 -0500

    Place counter code in function

--- src/counter.php
+++ src/counter.php
@@ -1,15 +1,17 @@
 <?php
 
-//opens countlog.txt to read the number of hits
-$datei = fopen("countlog.txt","r");
-$count = fgets($datei,1000);
-fclose($datei);
-$count=$count+1;
-echo "$count";
+function counter($file) {
+    //opens countlog.txt to read the number of hits
+    $datei = fopen($file,"r");
+    $count = fgets($datei,1000);
+    fclose($datei);
+    $count=$count+1;
 
-// opens countlog.txt to change new hit number
-$datei = fopen("countlog.txt","wb");
-fwrite($datei, $count);
-fclose($datei);
+    // opens countlog.txt to change new hit number
+    $datei = fopen($file,"wb");
+    fwrite($datei, $count);
+    fclose($datei);
 
+    return $count;
+}
 ?>
