46fcca6ced20244395ecb5d88a9bac028597aee3
Ben Piper <ben@benpiper.com>
2018-03-06 19:12:29 -0500

    Display instance and client hostnames

--- src/index.php
+++ src/index.php
@@ -1,10 +1,12 @@
 <?php
 include("counter.php");
 
-echo "Hostname: " . gethostname() . "<br>";
+echo "Local hostname: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/local-hostname') . "<br>";
+echo "Public hostname: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-hostname') . "<br>";
 echo "Public IP: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-ipv4') . "<br>";
 echo "Host headers: " . $_SERVER['HTTP_HOST'] . "<br>";
 echo "Client IP: " . $_SERVER['REMOTE_ADDR'] . "<br>";
+echo "Client hostname: " . $_SERVER['REMOTE_HOST'] . "<br>";
 echo "Hits: " . counter("countlog.txt");
 //phpinfo();
 ?>
