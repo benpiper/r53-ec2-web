b19b17d46682eeb1a9915703977bca6e4168dc8e
Ben Piper <ben@benpiper.com>
2018-03-06 21:39:37 -0500

    Get client hostname using gethostbyaddr

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@ echo "Public hostname: " . file_get_contents('http://169.254.169.254/2016-09-02/
 echo "Public IP: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-ipv4') . "<br>";
 echo "Host headers: " . $_SERVER['HTTP_HOST'] . "<br>";
 echo "Client IP: " . $_SERVER['REMOTE_ADDR'] . "<br>";
-echo "Client hostname: " . $_SERVER['REMOTE_HOST'] . "<br>";
+echo "Client hostname: " . gethostbyaddr ($_SERVER['REMOTE_ADDR']) . "<br>";
 echo "Hits: " . counter("countlog.txt");
 //phpinfo();
 ?>
