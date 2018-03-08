dc6bc8906f0baf334534c534d3b01da36f786eda
Ben Piper <ben@benpiper.com>
2018-03-08 12:36:53 -0500

    Add table

--- src/index.php
+++ src/index.php
@@ -10,7 +10,7 @@ if (getenv("DBHOSTNAME") === false) {
 $dbip = gethostbyname($dbhostname);
 $dbstatus = "NOT CONNECTED";
 
-//create tcp client
+//open tcp connection
 $fp = fsockopen($dbip, 80, $errno, $errstr, 2);
 if($fp) {
     $dbstatus = "CONNECTED";
@@ -22,18 +22,17 @@ $publicip = file_get_contents('http://169.254.169.254/2016-09-02/meta-data/publi
 
 echo "<head><title>$publicip | $hits</title><style>";
 include("style.css");
-echo "</style></head><body>";
-
-echo "Local hostname: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/local-hostname') . "<br>";
-echo "Public hostname: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-hostname') . "<br>";
-echo "Public IP: $publicip<br>";
-echo "Availability zone: ". file_get_contents('http://169.254.169.254/2016-09-02/meta-data/placement/availability-zone') . "<br>";
-echo "Host headers: " . $_SERVER['HTTP_HOST'] . "<br>";
-echo "Database hostname: " . $dbhostname . "<br>";
-echo "Database IP: $dbip<br>";
-echo "Database status: $dbstatus<br>";
-echo "Client IP: " . $_SERVER['REMOTE_ADDR'] . "<br>";
-echo "Client hostname: " . gethostbyaddr ($_SERVER['REMOTE_ADDR']) . "<br>";
-echo "Hits: $hits";
-echo "</body>";
+echo "</style></head><body><table>";
+echo "<tr><td>Local hostname:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/local-hostname') . "</td></tr><br>";
+echo "<tr><td>Public hostname:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-hostname') . "</td></tr><br>";
+echo "<tr><td>Public IP:</td><td>$publicip</td></tr><br>";
+echo "<tr><td>Availability zone:</td><td>". file_get_contents('http://169.254.169.254/2016-09-02/meta-data/placement/availability-zone') . "</td></tr><br>";
+echo "<tr><td>Host headers:</td><td>" . $_SERVER['HTTP_HOST'] . "</td></tr><br>";
+echo "<tr><td>Database hostname:</td><td>" . $dbhostname . "</td></tr><br>";
+echo "<tr><td>Database IP:</td><td>$dbip</td></tr><br>";
+echo "<tr><td>Database status:</td><td>$dbstatus</td></tr><br>";
+echo "<tr><td>Client IP:</td><td>" . $_SERVER['REMOTE_ADDR'] . "</td></tr><br>";
+echo "<tr><td>Client hostname:</td><td>" . gethostbyaddr ($_SERVER['REMOTE_ADDR']) . "</td></tr><br>";
+echo "<tr><td>Hits:</td><td>$hits</td></tr><br>";
+echo "</table></body>";
 ?>
