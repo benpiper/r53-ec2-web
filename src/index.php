906d51244b1d085a1c01c78eb19ef987c22da5ca
Ben Piper <ben@benpiper.com>
2018-03-08 13:05:16 -0500

    Remove breaks and add CSS for table

--- src/index.php
+++ src/index.php
@@ -23,16 +23,16 @@ $publicip = file_get_contents('http://169.254.169.254/2016-09-02/meta-data/publi
 echo "<head><title>$publicip | $hits</title><style>";
 include("style.css");
 echo "</style></head><body><table>";
-echo "<tr><td>Local hostname:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/local-hostname') . "</td></tr><br>";
-echo "<tr><td>Public hostname:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-hostname') . "</td></tr><br>";
-echo "<tr><td>Public IP:</td><td>$publicip</td></tr><br>";
-echo "<tr><td>Availability zone:</td><td>". file_get_contents('http://169.254.169.254/2016-09-02/meta-data/placement/availability-zone') . "</td></tr><br>";
-echo "<tr><td>Host headers:</td><td>" . $_SERVER['HTTP_HOST'] . "</td></tr><br>";
-echo "<tr><td>Database hostname:</td><td>" . $dbhostname . "</td></tr><br>";
-echo "<tr><td>Database IP:</td><td>$dbip</td></tr><br>";
-echo "<tr><td>Database status:</td><td>$dbstatus</td></tr><br>";
-echo "<tr><td>Client IP:</td><td>" . $_SERVER['REMOTE_ADDR'] . "</td></tr><br>";
-echo "<tr><td>Client hostname:</td><td>" . gethostbyaddr ($_SERVER['REMOTE_ADDR']) . "</td></tr><br>";
-echo "<tr><td>Hits:</td><td>$hits</td></tr><br>";
+echo "<tr><td>Local hostname:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/local-hostname') . "</td></tr>";
+echo "<tr><td>Public hostname:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-hostname') . "</td></tr>>";
+echo "<tr><td>Public IP:</td><td>$publicip</td></tr>";
+echo "<tr><td>Availability zone:</td><td>". file_get_contents('http://169.254.169.254/2016-09-02/meta-data/placement/availability-zone') . "</td></tr>";
+echo "<tr><td>Host headers:</td><td>" . $_SERVER['HTTP_HOST'] . "</td></tr>";
+echo "<tr><td>Database hostname:</td><td>" . $dbhostname . "</td></tr>";
+echo "<tr><td>Database IP:</td><td>$dbip</td></tr>";
+echo "<tr><td>Database status:</td><td>$dbstatus</td></tr>";
+echo "<tr><td>Client IP:</td><td>" . $_SERVER['REMOTE_ADDR'] . "</td></tr>";
+echo "<tr><td>Client hostname:</td><td>" . gethostbyaddr ($_SERVER['REMOTE_ADDR']) . "</td></tr>";
+echo "<tr><td>Hits:</td><td>$hits</td></tr>";
 echo "</table></body>";
 ?>
