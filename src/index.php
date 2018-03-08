9527511887be1d6d4e9fc85259c3568821ce1698
Ben Piper <ben@benpiper.com>
2018-03-08 00:27:44 -0500

    Replace http request with tcp connection

--- src/index.php
+++ src/index.php
@@ -7,14 +7,24 @@ if (getenv("DBHOSTNAME") === false) {
     $dbhostname = getenv("DBHOSTNAME");
 }
 
+$dbip = gethostbyname($dbhostname);
+$dbstatus = "NOT CONNECTED";
+
+//create tcp client
+$fp = fsockopen($dbip, 80, $errno, $errstr, 2);
+if($fp) {
+    $dbstatus = "CONNECTED";
+    fclose($fp);
+}
+
 echo "Local hostname: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/local-hostname') . "<br>";
 echo "Public hostname: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-hostname') . "<br>";
 echo "Public IP: " . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-ipv4') . "<br>";
 echo "Availability zone: ". file_get_contents('http://169.254.169.254/2016-09-02/meta-data/placement/availability-zone') . "<br>";
 echo "Host headers: " . $_SERVER['HTTP_HOST'] . "<br>";
 echo "Database hostname: " . $dbhostname . "<br>";
-echo "Database IP: " . gethostbyname($dbhostname) . "<br>";
-echo "Database status: " . file_get_contents("http://$dbhostname") . "<br>";
+echo "Database IP: $dbip<br>";
+echo "Database status: $dbstatus<br>";
 echo "Client IP: " . $_SERVER['REMOTE_ADDR'] . "<br>";
 echo "Client hostname: " . gethostbyaddr ($_SERVER['REMOTE_ADDR']) . "<br>";
 echo "Hits: " . counter("countlog.txt");
