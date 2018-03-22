34117c089a1e02f49c19d38892c11812b3b214f0
Ben Piper <ben@benpiper.com>
2018-03-21 20:51:22 -0400

    Change "hostname" to "domain name"

--- src/index.php
+++ src/index.php
@@ -26,18 +26,18 @@ include("style.css");
 echo "</style>";
 if (getenv("HEAP_APP_ID") !== false) { include("tracker.php"); }
 echo "</head><body><table>";
-echo "<tr><td>Local hostname:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/local-hostname') . "</td></tr>";
-echo "<tr><td>Public hostname:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-hostname') . "</td></tr>";
+echo "<tr><td>Local domain name:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/local-hostname') . "</td></tr>";
+echo "<tr><td>Public domain name:</td><td>" . file_get_contents('http://169.254.169.254/2016-09-02/meta-data/public-hostname') . "</td></tr>";
 echo "<tr><td>Public IP:</td><td>$publicip</td></tr>";
 echo "<tr><td>Availability zone:</td><td>". file_get_contents('http://169.254.169.254/2016-09-02/meta-data/placement/availability-zone') . "</td></tr>";
 if ($dbhostname !== false) {
-    echo "<tr><td>Database hostname:</td><td>" . $dbhostname . "</td></tr>";
+    echo "<tr><td>Database domain name:</td><td>" . $dbhostname . "</td></tr>";
     echo "<tr><td>Database IP:</td><td>$dbip</td></tr>";
     echo "<tr><td>Database status:</td><td>$dbstatus</td></tr>";
 }
 echo "<tr><td>Client IP:</td><td>" . $_SERVER['REMOTE_ADDR'] . "</td></tr>";
-echo "<tr><td>Client hostname:</td><td>" . gethostbyaddr ($_SERVER['REMOTE_ADDR']) . "</td></tr>";
-echo "<tr><td>Host headers:</td><td>" . $_SERVER['HTTP_HOST'] . "</td></tr>";
+echo "<tr><td>Client domain name:</td><td>" . gethostbyaddr ($_SERVER['REMOTE_ADDR']) . "</td></tr>";
+echo "<tr><td>Host header:</td><td>" . $_SERVER['HTTP_HOST'] . "</td></tr>";
 echo "<tr><td>Hits:</td><td><span class=hits>$hits</span></td></tr>";
 echo "</table>";
 if (getenv("HEAP_APP_ID") !== false) { echo "<script>heap.track('DNS Response', {publicip: '$publicip'});</script>"; }
